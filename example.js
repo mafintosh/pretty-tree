e3e436352b625d8ac42b7bcac933b4d9b69e4685
Mathias Buus <mathiasbuus@gmail.com>
2014-01-04 19:02:21 +0100

    added basic example

---
+++ example.js
@@ -0,0 +1,61 @@
+var tree = require('./index');
+
+var str = tree({
+	label: '(root)', // the label of this node
+	nodes: [{
+		label: '(child)',
+		leaf: {
+			hello: 'world',
+			hej: 'verden'
+		}
+	}]
+});
+
+console.log(str);
+
+var str = tree({
+	label: '(root)',
+	nodes: [
+		{
+			label: '(child-1)',
+			leaf: {
+				foo: 'bar',
+				bar: true,
+				foobar:'baz',
+				nested: {
+					value: 'her',
+					count: 42
+				},
+				nested2: {
+					value: 'her',
+					count: 42
+				},
+				list: [
+					11,
+					22,
+					32,
+					{
+						foo: 'bar',
+						bar: 'baz'
+					},
+					{
+						foo: 'meh'
+					},
+					{
+						foo1: 'bar',
+						bar2: 'baz'
+					}
+				]
+			}
+		},
+		{
+			leaf: 'hello'
+		},
+		{
+			label: '(child-2)',
+			leaf: true
+		}
+	]
+});
+
+console.log(str);
